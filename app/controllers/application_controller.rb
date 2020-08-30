1ccdfd872a2c2499fdf3a90aaf32ac8e590dbde1
m-tatsuyama <mt.tatsuyama@gmail.com>
2020-08-30 15:14:22 +0900

    deviseに関するビューファイルの配置

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :first_name, :last_name, :first_name_reading, :last_name_reading, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
 
   def basic_auth
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
 
-  resources :items, only: [:index, :new]
-  resources :users, only: [:index, :create, :edit, :update, :show]
+  resources :items
 end
