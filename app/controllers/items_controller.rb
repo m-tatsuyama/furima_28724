e1048ad011fb5adbc4002260134220c083e4badd
m-tatsuyama <mt.tatsuyama@gmail.com>
2020-09-01 11:35:30 +0900

    出品ページへの遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,13 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :show]
   before_action :move_to_index, except: [:index, :show]
 
+  def index
+    @items = Item.all
+  end
+
+  def new
+  end
+
   private
 
   def item_params
