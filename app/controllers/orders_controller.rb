51bedb3bd9e5d0255034fd1ce077a0a7ff29e0cc
m-tatsuyama <mt.tatsuyama@gmail.com>
2020-09-14 16:36:00 +0900

    購入機能　修正１

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,12 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :item_params, only[:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       pay_item
@@ -19,6 +18,10 @@ class OrdersController < ApplicationController
   end
 
   private
+
+  def item_params
+    @item = Item.find(params[:item_id])
+  end
 
   def order_params
     params.require(:order_address).permit(:token, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone, :item_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -14,10 +14,7 @@ const pay = () => {
       exp_month: formData.get("order_address[exp_month]"),
       exp_year: `20${formData.get("order_address[exp_year]")}`,
     };
-    // console.log(card)
     Payjp.createToken(card, (status, response) => {
-      // console.log(response);
-      // debugger
       if (status == 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
